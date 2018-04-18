598a45b440ed20a012df1cd24f7ea0ccca593be1
SiYoungOh(ohahohah) <ohahohah.dev@gmail.com>
2018-04-18 20:39:06 +0900

    ST-5 Noti 수가 맞는 걸 확인

---
+++ src/main/java/net/slipp/todo_list/CountableNotiManager.java
@@ -0,0 +1,21 @@
+package net.slipp.todo_list;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+public class CountableNotiManager extends NotiManager {
+
+  private int callCount = 0;
+
+  public int getCallCount() {
+    return this.callCount;
+  }
+
+  @Override
+  public void notify(String title) throws RuntimeException {
+    this.callCount++;
+    super.notify(title);
+  }
+}
