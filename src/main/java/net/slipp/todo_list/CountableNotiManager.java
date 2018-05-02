8bcc1cd98f84d8a5a6842d0d275bbb455c45022a
임도형 <dhrim@ex-em.com>
2018-05-02 21:03:37 +0900

    [긴급] 테스트 실패하는 master 픽스. - CountableNotiManager의 @Primary를 삭제. - NotiManager의 @Component를 삭제

--- src/main/java/net/slipp/todo_list/CountableNotiManager.java
+++ src/main/java/net/slipp/todo_list/CountableNotiManager.java
@@ -3,7 +3,6 @@ package net.slipp.todo_list;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Primary
 @Component
 public class CountableNotiManager extends NotiManager {
 
