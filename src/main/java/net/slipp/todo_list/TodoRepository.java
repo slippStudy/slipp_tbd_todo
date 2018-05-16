92de221424e6f873a67708193207a0003167aa26
ohahohah <ohahohah.dev@gmail.com>
2018-05-16 18:54:45 +0900

    [ST-6] TodoManager 삭제 기능 구현 - TodoManager.delete(Todo) 메소드 구현 - Testcase 분리

--- src/main/java/net/slipp/todo_list/TodoRepository.java
+++ src/main/java/net/slipp/todo_list/TodoRepository.java
@@ -9,5 +9,6 @@ public class TodoRepository {
 
     public Todo store(Todo todo) throws IllegalArgumentException, RepositoryFailedException { return null; }
 
+    public Todo store(Todo todo, boolean should_delete) throws IllegalArgumentException { return null; }
 
 }
